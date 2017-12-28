152bf734bc384c1a4a5ec648897a12933a9bc82a
shadowmage45 <shadowmage45@hotmail.com>
2017-12-28 16:09:37 -0700

    umm..erm...NFC wtf the problem even is... why am I writing code for others for free?

---
+++ Plugin/SSTUTools/KSPShaderTools/Module/KSPFairingTextureSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSPShaderTools
+{
+    public class KSPFairingTextureSwitch : KSPTextureSwitch
+    {
+
+        public void Start()
+        {
+
+        }
+
+    }
+}
