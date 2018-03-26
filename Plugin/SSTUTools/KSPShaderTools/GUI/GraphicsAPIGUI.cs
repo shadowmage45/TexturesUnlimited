8ba9a68adca5a84a882359b30a83deef396a5056
Shadowmage <shadowmage45@hotmail.com>
2018-03-26 13:05:21 -0600

    implement gfx-api check GUI internals; just a simple warning popup window

--- Plugin/SSTUTools/KSPShaderTools/GUI/GraphicsAPIGUI.cs
+++ Plugin/SSTUTools/KSPShaderTools/GUI/GraphicsAPIGUI.cs
@@ -9,31 +9,54 @@ namespace KSPShaderTools
     public class GraphicsAPIGUI : MonoBehaviour
     {
 
-        private Rect windowRect;
+        private Rect windowRect = new Rect(Screen.width - 500, 40, 480, 320);
         private int instanceID;
 
-        public void OnAwake()
-        {
+        private bool open = false;
 
+        public void Awake()
+        {
+            instanceID = GetInstanceID();
         }
 
         public void Start()
         {
-
+            //noop
         }
 
         public void Destroy()
         {
-
+            //noop
         }
 
         public void OnGUI()
         {
+            if (!open) { return; }
+            windowRect = GUI.Window(instanceID, windowRect, drawWindow, "Textures Unlimited - GFX API Check");
+        }
 
+        private void drawWindow(int id)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label("Graphics API Detected: "+ SystemInfo.graphicsDeviceType);
+            GUILayout.Label("is unsupported by Textures Unlimited, and may not render correctly.");
+            GUILayout.Label("For best results, use the -force-glcore command line option to start KSP using the OpenGL Core graphics API");
+            if (GUILayout.Button("Acknowledge & Close"))
+            {
+                closeGUI();
+            }
+            GUILayout.EndVertical();
+            GUI.DragWindow();
+        }
+
+        internal void openGUI()
+        {
+            open = true;
         }
 
         private void closeGUI()
         {
+            open = false;
             TexturesUnlimitedLoader.INSTANCE.removeAPICheckGUI();
         }
 
