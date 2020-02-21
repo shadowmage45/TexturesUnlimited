28fb82a46054e91e761c30d5a85624c7e5e84a14
Shadowmage <shadowmage45@hotmail.com>
2020-02-21 14:45:39 -0700

    ..

--- Plugin/SSTUTools/KSPShaderTools/Util/ShaderProperty.cs
+++ Plugin/SSTUTools/KSPShaderTools/Util/ShaderProperty.cs
@@ -271,7 +271,7 @@ namespace KSPShaderTools
         {
             string[] vals = line.Split(',');
             this.textureName = vals[1].Trim();
-            this.normal = this.textureName == "_BumpMap";
+            this.normal = textureName == "_BumpMap";
         }
 
         protected override void applyInternal(Material mat)
